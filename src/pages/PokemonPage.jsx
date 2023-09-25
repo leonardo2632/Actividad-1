71eeabba54c3482d5ec3b2e846abef5bf2cbe53b
leonardo2632 <leo442632@gmail.com>
2023-09-24 20:04:09 -0600

    segundo repositorio

---
+++ src/pages/PokemonPage.jsx
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader } from '../components';
+import { PokemonContext } from '../context/PokemonContext';
+import { primerMayuscula } from '../helper/helper';
+
+export const PokemonPage = () => {
+	const { getPokemonByID } = useContext(PokemonContext);
+
+	const [loading, setLoading] = useState(true);
+	const [pokemon, setPokemon] = useState({});
+
+	const { id } = useParams();
+
+	const fetchPokemon = async id => {
+		const data = await getPokemonByID(id);
+		setPokemon(data);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		fetchPokemon(id);
+	}, []);
+
+}
